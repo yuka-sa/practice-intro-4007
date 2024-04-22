2886e190e45e659a2796c42d28ef407ba3fc688b
Sato Yuka <165768544+yuka-sa@users.noreply.github.com>
2024-04-22 15:06:00 +0900

    移動ボタン追加

--- app/entry.js
+++ app/entry.js
@@ -7,3 +7,10 @@ scalingButton.on('click', () => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
+
+const movingButton = $('#moving-button');
+
+movingButton.on('click', () => {
+  block.animate({ marginLeft: '500px' }, 500);
+  block.animate({ marginLeft: '20px' }, 1000);
+});
